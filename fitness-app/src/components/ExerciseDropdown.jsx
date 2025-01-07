5389421ee1defb3f5ae2d8eb6be27cc4905f3e43
linapau <lina.pau456@gmail.com>
2025-01-08 00:29:02 +0100

    added a top margin to  select exercise divhe

--- fitness-app/src/components/ExerciseDropdown.jsx
+++ fitness-app/src/components/ExerciseDropdown.jsx
@@ -1,44 +1,3 @@
-/*import React, { useState } from 'react';
-
-const ExerciseDropdown = ({ availableExercises, onSelectExercise }) => {
-  const [selectedExercise, setSelectedExercise] = useState('');
-
-  const handleAdd = () => {
-    if (selectedExercise) {
-      const exercise = availableExercises.find((ex) => ex.name === selectedExercise);
-      console.log(exercise);
-      console.log(exercise.name);
-      onSelectExercise(exercise); //passing the variable to DayWorkout
-      setSelectedExercise(""); //clean dropdown
-    }
-  };
-
-  return (
-    <div className="mt-4">
-      <select
-        value={selectedExercise}
-        onChange={(e) => setSelectedExercise(e.target.value)}
-        className="w-full p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-200 focus:ring focus:ring-blue-500"
-      >
-        <option value="">Select Exercise</option>
-        {availableExercises.map((exercise, index) => (
-          <option key={index} value={exercise.name}>
-            {exercise.name}
-          </option>
-        ))}
-      </select>
-      <button
-        onClick={handleAdd}
-        className="mt-2 w-full px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md transition-all"
-      >
-        Add Exercise
-      </button>
-    </div>
-  );
-};
-
-export default ExerciseDropdown;*/
-
 import React, { useState } from 'react';
 
 const ExerciseDropdown = ({ availableExercises, onSelectExercise }) => {
@@ -58,7 +17,7 @@ const ExerciseDropdown = ({ availableExercises, onSelectExercise }) => {
   };
 
   return (
-    <div className="flex flex-col space-y-2">
+    <div className="flex flex-col space-y-2 mt-2">
       <select
         value={selectedExercise}
         onChange={(e) => setSelectedExercise(e.target.value)}
